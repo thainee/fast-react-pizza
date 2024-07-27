00e8cd03bb0a85554fc00fabc241aff00fa4a242
thainee <thaivh9@fpt.com>
2024-07-27 15:40:26 +0700

    feat(router): new way implemeting react-router 6.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import Order from "./features/order/Order"
+import CreateOrder from "./features/order/CreateOrder"
+import OrderItem from "./features/order/OrderItem"
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/menu',
+      element: <Menu />
+    },
+    {
+      path: '/cart',
+      element: <Cart />
+    },
+    {
+      path: '/order',
+      element: <Order />
+    },
+    {
+      path: '/order/new',
+      element: <CreateOrder />
+    },
+    {
+      path: '/order/:id',
+      element: <OrderItem />
+    },
+  ])
+
   return (
-    <div>Hello Vite!</div>
+    <RouterProvider router={router} />
   )
 }
 
