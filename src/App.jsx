91976abfd0f41326e4ae0ce7e0c8eaa6f3f1bc4b
thainee <thaivh9@fpt.com>
2024-07-27 20:24:38 +0700

    feat(loader): fetching menu data with react router loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from "./features/menu/Menu"
+import Menu, { loader as menuLoader } from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
 import Order from "./features/order/Order"
 import CreateOrder from "./features/order/CreateOrder"
@@ -18,7 +18,8 @@ function App() {
         },
         {
           path: '/menu',
-          element: <Menu />
+          element: <Menu />,
+          loader: menuLoader
         },
         {
           path: '/cart',
