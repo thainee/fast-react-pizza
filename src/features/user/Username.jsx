cc3beb202d00ea4f40b5acf7dddb5817950db10c
thainee <thaivh9@fpt.com>
2024-07-29 10:32:58 +0700

    style: learning flex with tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Thai Vo</div>;
+  return <div className="hidden text-sm font-semibold md:block">Thai Vo</div>;
 }
 
 export default Username;
