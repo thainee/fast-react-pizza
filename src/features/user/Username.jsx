f356e4e7fb02df85008d2407d7c7810f127df673
thainee <thaivh9@fpt.com>
2024-07-29 09:51:30 +0700

    style: learning box model spacing, borders and display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Thai Vo</div>;
+  return <div className="hidden text-sm font-semibold">Thai Vo</div>;
 }
 
 export default Username;
