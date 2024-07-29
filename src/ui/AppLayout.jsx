42c4b84ff09300d2206705e33ecb7c668ca185f3
thainee <thaivh9@fpt.com>
2024-07-29 13:40:05 +0700

    style: learning using css grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,24 +1,26 @@
-import Header from "./Header"
-import CartOverview from "../features/cart/CartOverview"
-import { Outlet, useNavigation } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader />}
-            <Header />
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
 
-            <main>
-                <Outlet />
-            </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-            <CartOverview />
-        </div>
-    )
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
