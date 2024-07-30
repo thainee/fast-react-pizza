3761a5be9aab64b211369faea24e5c73d8242255
thainee <thaivh9@fpt.com>
2024-07-30 16:28:30 +0700

    style(cart): styling the cart, add secondary type button

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,15 +1,17 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
+    <li className="items-center py-3 sm:flex sm:justify-between">
       <p>
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-end justify-between sm:items-center sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
