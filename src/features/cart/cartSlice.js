10b9e1a5bb83d472dc4fea1d06572001dd855498
thainee <thaivh9@fpt.com>
2024-08-01 00:16:30 +0700

    feat(cart page): building the cart page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -63,3 +63,5 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCart = (state) => state.cart.cart;
