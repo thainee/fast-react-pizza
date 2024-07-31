0950181fea7d2082a1bff4517d79354770db1aed
thainee <thaivh9@fpt.com>
2024-08-01 00:37:14 +0700

    feat(delete cart item): create component delete cart item to reuse in menuitem and cartitem, and it can delete from the cart

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,5 +1,5 @@
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import DeleteCartItem from "./DeleteCartItem";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
@@ -11,7 +11,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-end justify-between sm:items-center sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteCartItem pizzaId={pizzaId} />
       </div>
     </li>
   );
